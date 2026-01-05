9228c3392bdf1062e43308da713d440922a48d32
adityakumar <adityawrs1@gmail.com>
2026-01-05 17:11:26 +0530

    updated some files

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import { createServer } from 'node:http';
-import fs from 'node:fs/promises';
+//import fs from 'node:fs/promises';
+//import * as fs from 'node:fs/promises';
+import fs from 'node:fs'
 
 const server = createServer(async(req,res)=>{
     //console.log(`request recieved`);
@@ -8,15 +10,43 @@ const server = createServer(async(req,res)=>{
     //res.writeHead(200,{'content-type':'text/html'})
     //console.log('req',req);
     if(req.url==='/'){
-        res.writeHead(200,{'content-type':'text/html'});
-        const data =await fs.readFile('./index.html');
-        res.end(data);
+        //res.writeHead(200,{'content-type':'text/html'});
+        //const data =await fs.readFile('./index.html');
+        const datastream = fs.createReadStream('./index.html')
+
+        datastream.pipe(res);
+        // datastream.on('data',(chunk)=>{
+        //     res.write(chunk);
+        // });
+
+        // datastream.on('end',()=>{
+        //     res.end();
+        // });
+        //res.end(data);
+        
     }else if(req.url==='/about'){
         res.writeHead(200,{'content-type':'text/html'});
         res.end('<h1>helloo aditya kumar </h1>');
+    }else if (req.url === '/expenses'){
+        //Api 
+        //Create an expense
+        //POST
+
+
+        if(req.method === 'POST'){
+            //READ DATA from reques
+            let buffer =''
+            //store it in json database
+            req.on('data',(chunk)=>{
+                console.log('chunk' , chunk);
+                //buffer = buffer + data.toString();
+            })
+        }else if(req.method === 'Get'){
+            // read data from json db
+            // return the data to client
+        }
     }
 
-    
 })
 
 server.listen(3000,()=>{
